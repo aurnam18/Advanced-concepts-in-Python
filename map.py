689d64bd33df9f6ce000989fc28bc18c095d7143
Aurnam18 <Aurnam18@gmail.com>
2020-07-18 22:49:21 -0500

    More with map

--- map.py
+++ map.py
@@ -13,11 +13,33 @@ numbers = (1, 2, 3, 4)
 result = map(addition, numbers)
 print(list(result))
 
+#Use map to square numbers in list
+def square(n):
+    return n ** 2
+lst1=[10, 20, 30, 40, 50, 60]
+lst2=map(square, lst1)
+
+print(lst2)
+
 # Double all numbers using map and lambda
 # Going through each number in numbers
 numbers = (1, 2, 3, 4)
 result = map(lambda x: x + x, numbers)
 print(list(result))
+
+#Puts hello before each value in the list
+
+lst1=["Jane", "Lee", "Will", "Brie"]
+lst2=map(lambda x: "Hello, " + x, lst1)
+
+print(lst2)
+
+#Converting uppercase A to lowercase a, lowercase a stays lowercase
+lst1=["Alaska", "Alabama", "Arizona", "Arkansas", "Colorado", "Montana", "Nevada"]
+lst2=list(map(lambda x: x.lower().count("a"), lst1))
+
+
+print(lst2)
 
 # Add two lists using map and lambda
 
@@ -32,4 +54,4 @@ l = ['sat', 'bat', 'cat', 'mat']
 
 # map() can listify the list of strings individually
 test = list(map(list, l))
-print(test) 
+print(test)

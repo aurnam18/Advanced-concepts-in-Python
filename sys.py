1bfb4020df570d63f3eb0b1a419c3060acefbcdc
Aurnam18 <Aurnam18@gmail.com>
2020-07-22 19:29:33 -0500

    Using sys in Python

---
+++ sys.py
@@ -0,0 +1,9 @@
+import sys
+#returns a list of command line arguments passed to a Python script
+print(sys.argv)
+#largest integer variable can take
+print(sys.maxsize)
+#search path for all python modules
+print(sys.path)
+#version number of current python interpreter
+print(sys.version)
